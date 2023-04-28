29eccfb0c38511f763b9f5debb0d481a0f8def44
ThabangLeeuw <thabangleeuw97@gmail.com>
2023-04-29 01:54:05 +0200

    Sorting algorithms tasks

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+
+/**
+ * cocktail_sort_list - Name of prototype
+ * Return: Nothing because prototype is void
+ */
+void cocktail_sort_list(listint_t **list)
+{
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# 0x1B. C - Sorting algorithms & Big O
+
+## Author
+Imraan Thabang Leeuw - thabangleeuw97@gmail.com
